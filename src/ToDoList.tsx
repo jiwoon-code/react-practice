ed67926394d50938dd19878405686985826a19e2
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2025-03-24 22:46:21 +0900

    react hook form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function ToDoList() {
+/* function ToDoList() {
   const [toDo, setToDo] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
@@ -17,6 +18,20 @@ function ToDoList() {
       <h1>내가 가고 싶은 나라들</h1>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} placeholder="name" />
+        <button>가자!</button>
+      </form>
+    </div>
+  );
+}
+*/
+
+function ToDoList() {
+  const { register } = useForm();
+  return (
+    <div>
+      <h1>내가 가고 싶은 나라들</h1>
+      <form>
+        <input {...register("toDo")} placeholder="name" />
         <button>가자!</button>
       </form>
     </div>
